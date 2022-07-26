72cd3347a1aa55a56ad0dae6c5db496130747296
git stash <git@stash>
2022-07-26 09:12:06 +0200

    index on master: 508c5c8 debugging available again in development mode

--- src/app/models/Product.ts
+++ src/app/models/Product.ts
@@ -1,10 +1,7 @@
 export interface Product {
-  id: number;
   name: string;
-  price: number;
   description: string;
-  imageUrl: string;
-  isAvailable: boolean;
-  isPopular: boolean;
-  category: string;
+  price: number;
+  createdAt: Date;
+  ModifiedAt: Date;
 }
