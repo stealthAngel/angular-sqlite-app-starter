ae7f3b8ceb730a233e0233c8af762b30838ea968
Mohammed Belhaj <m.belhaj_zakelijk@hotmail.com>
2022-08-12 20:35:06 +0200

    u

--- src/app/pages/missions/missions.page.ts
+++ src/app/pages/missions/missions.page.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
-import { Subject } from 'rxjs';
+import { ChangeDetectorRef, Component, OnInit, QueryList, TemplateRef, ViewChild, ViewChildren } from '@angular/core';
 import { FlipperComponent } from 'src/app/components/flipper/flipper.component';
 import { Mission } from 'src/app/models/Mission';
 import { MissionRepository } from 'src/app/repositories/mission.repository';
-
+import { Swiper } from 'swiper';
 @Component({
   selector: 'app-missions',
   templateUrl: './missions.page.html',
@@ -12,12 +11,15 @@ import { MissionRepository } from 'src/app/repositories/mission.repository';
 export class MissionsPage implements OnInit {
 
   missions: Mission[];
-  selectedSegment: string = 'Notepad View';
+  segments: string[] = ['Card View', 'Notepad View'];
+  selectedSegment: string = this.segments[0];
   //view children of appflipper
   @ViewChildren(FlipperComponent) flippers: QueryList<FlipperComponent>;
 
+  swiper: Swiper;
 
-  constructor(private missionRepository: MissionRepository) { }
+
+  constructor(private missionRepository: MissionRepository, private changeDetectorRef: ChangeDetectorRef) { }
 
   ngOnInit() {
     this.get();
@@ -29,7 +31,7 @@ export class MissionsPage implements OnInit {
   }
 
   segmentChanged(ev: any) {
-    console.log('Segment changed', ev);
+    this.swiper.slideTo(this.segments.indexOf(ev.detail.value));
   }
 
   onSearchChange($event) {
@@ -47,8 +49,18 @@ export class MissionsPage implements OnInit {
   onEditMissionClick(id: number) {
     console.log("onEditMissionClick", id);
   }
+
   onDeleteMissionClick(id: number) {
     console.log("onDeleteMissionClick", id);
+  }
+
+  onSlideChange() {
+    this.selectedSegment = this.segments[this.swiper.activeIndex];
+    this.changeDetectorRef.detectChanges();
+  }
+
+  setSwiperInstance(swiper: Swiper) {
+    this.swiper = swiper;
   }
 
   async get() {
