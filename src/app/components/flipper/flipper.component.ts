1124fdf64ffb592a3215e8835765726926e07a18
Mohammed Belhaj <m.belhaj_zakelijk@hotmail.com>
2022-08-13 11:12:07 +0200

    u

--- src/app/components/flipper/flipper.component.ts
+++ src/app/components/flipper/flipper.component.ts
@@ -7,47 +7,21 @@ import {
   transition,
 } from '@angular/animations';
 
-export interface CardData {
-  state: 'default' | 'flipped' | 'matched';
-}
 
 @Component({
   selector: 'app-flipper',
   templateUrl: './flipper.component.html',
   styleUrls: ['./flipper.component.scss'],
-  animations: [
-    trigger('cardFlip', [
-      state('flipped', style({
-        transform: 'none'
-      })),
-      state('default', style({
-        transform: 'rotateY(180deg)'
-      })),
-      transition('default => flipped', [
-        animate('250ms')
-      ]),
-      transition('flipped => default', [
-        animate('250ms')
-      ])
-    ])
-  ]
 })
 export class FlipperComponent implements OnInit {
 
-  data: CardData = {
-    state: "default"
-  };
+  data: boolean = false;
 
   ngOnInit(): void {
   }
 
   cardClicked() {
-    if (this.data.state === "default") {
-      this.data.state = "flipped";
-    } else {
-      this.data.state = "default";
-    }
-    console.log(this.data.state);
+    this.data = !this.data;
   }
 }
 
